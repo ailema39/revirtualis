7e0a2c9d61f862f9224f666639be420a9b328f5c
Yomar Medina <ailema39@gmail.com>
2015-05-03 17:26:52 -0500

    Template Base

---
+++ include/footer.php
@@ -0,0 +1,69 @@
+<!--Begin footer -->
+	<footer class="footer-wrapper coming-footer-wrapper">
+		<!--Begin footer widget-->
+		<div class="container footer-widgets">
+			<div class="row">
+				<div class="col-sm-6 col-md-3 widget-news">
+					<h3 class="widget-title">Contact us</h3>
+	                <address class="widget-contacts">
+	                	<span class="widget-contacts__item"><i class="icon-circle icon-circle--thin fa fa-phone"></i>+ 44 1225 324 980</span>
+	                    <span class="widget-contacts__item"><i class="icon-circle icon-circle--thin fa fa-envelope"></i><a href="mailto:appic@gmail.com">appic@gmail.com</a></span>
+	                    <span class="widget-contacts__item"><i class="icon-circle icon-circle--thin fa fa-map-marker"></i>101 West Street, New York, NY 12345</span>
+               		</address>
+				</div>
+				<div class="col-sm-6 col-md-3 widget-social">
+					<h3 class="widget-title">Join us in social media</h3>
+					<ul class="social">
+			        	<li class="social__item custom-tooltip tooltip--info" title="pinterest">
+			        		<a class="icon-circle fa fa-pinterest" href="http://pinterest.com/oliagozha/" target="_blank"></a>
+			        	</li>
+			            <li class="social__item custom-tooltip tooltip--info" title="google">
+			            	<a class="icon-circle fa fa-google-plus" href="https://plus.google.com/u/0/100180333527158639883/posts" target="_blank"></a>
+			            </li>
+			            <li class="social__item custom-tooltip tooltip--info" title="linkedin">
+			            	<a class="icon-circle fa fa-linkedin" href="http://ua.linkedin.com/pub/olia-gozha/49/b91/268" target="_blank"></a>
+			            </li>
+			            <li class="social__item custom-tooltip tooltip--info" title="twitter">
+			            	<a class="icon-circle fa fa-twitter" href="http://twitter.com/@OliaGozha" target="_blank"></a>
+			            </li>
+			            <li class="social__item custom-tooltip tooltip--info" title="facebook">
+			            	<a class="icon-circle fa fa-facebook" href="http://www.facebook.com/people/@/http://www.facebook.com/olia.gozha" target="_blank"></a>
+			            </li>
+			            <li class="social__item custom-tooltip tooltip--info" title="vimeo">
+			            	<a class="icon-circle fa fa-vimeo-square" href="#" target="_blank"></a>
+			            </li>
+			            <li class="social__item custom-tooltip tooltip--info" title="youtube">
+			            	<a class="icon-circle fa fa-youtube" href="#" target="_blank"></a>
+			            </li>
+			            <li class="social__item custom-tooltip tooltip--info" title="skype">
+			            	<a class="icon-circle fa fa-skype" href="#" target="_blank"></a>
+			            </li>
+		        	</ul>
+				</div>
+				<div class="clearfix visible-sm"></div>
+				<div class="col-sm-6 col-md-3 widget-tweets">
+	            	<h3 class="widget-title">Latest Tweets</h3>
+	                <ul class="twitter_update_list" id="twitter-feed"></ul>
+				</div>
+				<div class="col-sm-6 col-md-3 widget-flickr">
+	            	<h3 class="widget-title">Flickr Stream</h3>
+	                <ul class="flickr" id="flickr"></ul>
+				</div>
+			</div>
+		</div>
+		<!--End footer widget -->
+
+		<!--Begin footer secondary -->
+		<div class="footer-secondary">
+
+            <div class="divider-circle"></div>
+
+			<div class="container">
+				<div class="copyright copyright--centered">
+		        	<small class="small">&copy; REVERTUALIS, 2015. All rights reserved. <a href="#">Done by Spooktech</a></small>
+		        </div>
+			</div>
+		</div>
+		<!--End footer secondary -->
+	</footer>
+	<!--End footer -->
